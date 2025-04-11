7497755c609ecbf4ce10f405be9fb4cfa6a1fcb4
Ethernaljoz <amoussoujoel500@gmail.com>
2025-04-11 12:11:17 +0000

    navbar redesign

--- assets/js/coffee.js
+++ assets/js/coffee.js
@@ -7,10 +7,19 @@
 // })
 function showSidebar(){
     const sidebar = document.querySelector(".sidebar");
+    const menu = document.querySelector(".menu-burger");
+    const menuClose = document.querySelector(".menu-close");
     sidebar.style.display = 'block';
+    menuClose.style.display = 'block';
+    menu.style.display = 'none';
+    sidebar.style.animation = 'sidebar-animation-enter 0.5s ease-out';
 }
 
 function hideSidebar(){
     const sidebar = document.querySelector(".sidebar");
+    const menu = document.querySelector(".menu-burger");
+    const menuClose = document.querySelector(".menu-close");
+    menuClose.style.display = 'none';
+    menu.style.display = 'block';
     sidebar.style.display = 'none';
 }
